0b4f37a3e2ca3450789d782fa835971f52cc82f8
ozkarta <ozbegi1@gmail.com>
2018-02-15 23:15:11 +0400

    User registration is implemented

---
+++ api/v1/models/user.model.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let userSchema = new mongoose.Schema({
+    firstName: {type: String, trim: true},
+    lastName: {type: String, trim: true},
+    email: {type: String, trim: true},
+    passwordHash: {type: String},
+    role: {type: String, enum: ['buyer']},
+    searches: [{type: String, trim: true}]
+}, {
+    timestamps: true
+});
+
+let userModel = mongoose.model('User', userSchema);
+
+exports.model = userModel;
+exports.schema = userSchema;
