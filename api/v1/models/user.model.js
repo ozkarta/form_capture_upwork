004f82772f5501e28fd2ed4ba75d98f1b409af9b
ozkarta <ozbegi1@gmail.com>
2018-02-17 14:16:14 +0400

    addresss is added

--- api/v1/models/user.model.js
+++ api/v1/models/user.model.js
@@ -4,6 +4,14 @@ let userSchema = new mongoose.Schema({
     firstName: {type: String, trim: true},
     lastName: {type: String, trim: true},
     email: {type: String, trim: true},
+    address : {
+        street: {type: String, trim: true},
+        city: {type: String, trim: true},
+        province: {type: String, trim: true},
+        country: {type: String, trim: true},
+        postalCode: {type: String, trim: true}
+    },
+
     passwordHash: {type: String},
     role: {type: String, enum: ['buyer']},
     searches: [{type: String, trim: true}]
