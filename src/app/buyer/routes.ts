1b3efe247e61f1800d545e8d3e14d3d591c93d6b
ozkarta <ozbegi1@gmail.com>
2018-02-18 20:31:13 +0400

    buyer messenger is added

--- src/app/buyer/routes.ts
+++ src/app/buyer/routes.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BuyerHomeComponent} from './home/home.component';
 import {BuyerAuthGuard} from './buyer-auth.guard';
+import {MessengerComponent} from '../shared/messenger/messenger.component';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,12 @@ export const routes: Routes = [
         component: BuyerHomeComponent,
         canActivate: [BuyerAuthGuard]
     },
+    {
+        path: 'buyer/chat',
+        component: MessengerComponent,
+        canActivate: [BuyerAuthGuard]
+    }
+
 ];
 
 @NgModule({
