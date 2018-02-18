9df26f45718a0de5532ba3a133de3ad784d62fa1
ozkarta <ozbegi1@gmail.com>
2018-02-18 20:28:32 +0400

    log in is implemented

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
+import {Observable}from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 import {AppService} from './app.service';
 
 @Injectable()
