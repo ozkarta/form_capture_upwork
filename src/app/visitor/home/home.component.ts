daf771aa8f6cdf5f584555f6aa1f4cc7adc73a50
ozkarta <ozbegi1@gmail.com>
2018-02-18 22:26:41 +0400

    new message modal is added, Needs styling

--- src/app/visitor/home/home.component.ts
+++ src/app/visitor/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../shared/service/user.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 @Component({
     templateUrl: './home.component.html',
     styleUrls: ['home.style.css']
@@ -9,10 +10,14 @@ export class VisitorHomeComponent implements OnInit {
     public searchOptions: any = {
         zip: ''
     };
+    public message: any = {
+
+    };
 
     public searchResult: any = null;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService,
+                private modalService: NgbModal) {
     }
 
     ngOnInit() {
@@ -29,5 +34,17 @@ export class VisitorHomeComponent implements OnInit {
                     console.dir(errorResponse);
                 }
             )
+    }
+
+    openNewMessageModal(modalWindow, user) {
+        this.modalService.open(modalWindow).result.then((result) => {
+            // this.closeResult = `Closed with: ${result}`;
+        }, (reason) => {
+            // this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        });
+    }
+
+    sendMessage() {
+
     }
 }
